a90702ed037bec680bae4ed042d28dd714970fbd
tomzacchia <tomzacchia@live.com>
2020-11-26 14:31:29 -0500

    Add support for .env

--- app.jsx
+++ app.jsx
@@ -1,46 +1,46 @@
-import React from 'react';
+import React from "react";
 
 /**
  * The API endpoints are availables as:
- * 
+ *
  * recipes:
  * get all -> GET -> /recipes/
  * get one -> GET -> /recipes/ID
  * create one -> POST -> /recipes/ {recipe: {name: 'abc', ingredients: [1,2,3]}}
  * update one -> PUT/PATCH -> /recipes/ID {recipe: {name: 'abc', ingredients: [1,2,3]}}
  * delete -> DELETE -> /recipes/ID
- * 
+ *
  * ingredients:
  * get all -> GET -> /ingredients/
  * get one -> GET -> /ingredients/ID
  * create one -> POST -> /ingredients/ {ingredient: {name: 'abc'}}
  * update one -> PUT/PATCH -> /ingredients/ID {ingredient: {name: 'abc'}}
  * delete -> DELETE -> /recipes/ID
- * 
+ *
  */
 
 const App = (props) => {
+  console.log(process.env.API_URL);
+  const [recipes, setRecipes] = React.useState(() => {
+    fetch(`/recipes`).then((resp) => {
+      resp.json().then((json) => {
+        setRecipes(json.recipes);
+      });
+    });
 
-    const [recipes, setRecipes] = React.useState(() => {
-        fetch('/recipes').then(resp => {
-            resp.json().then((json) => {
-                setRecipes(json.recipes)
-            })
-        })
-        
-        return []
-    })
+    return [];
+  });
 
-    return (
-        <div>Recipes:
-            <ul>
-                {recipes.map(recipe => {
-                    return (<li key={recipe.id}>{recipe.name}</li>)
-                })}
-            </ul>
-        </div>
-    )
+  return (
+    <div>
+      Recipes:
+      <ul>
+        {recipes.map((recipe) => {
+          return <li key={recipe.id}>{recipe.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
 
-}
-
-export default App
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,41 @@
-var path = require('path');
+// var path = require("path");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
 
-var config = {
-    entry: './main.js',
+var config = () => {
+  //https://medium.com/@trekinbami/using-environment-variables-in-react-6b0a99d83cf5
+  const env = dotenv.config().parsed;
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    entry: "./main.js",
     watch: true,
     output: {
-        path: __dirname,
-        filename: 'index.js'
+      filename: "index.js",
     },
     module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-react'
-                        ]
-                    }
-                }
-            }
-        ]
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        },
+      ],
     },
-    plugins: [],
+    plugins: [new webpack.DefinePlugin(envKeys)],
     devServer: {
-        contentBase: './dist',
-        overlay: true
-    }
+      contentBase: "./dist",
+      overlay: true,
+    },
+  };
 };
 
 module.exports = config;
