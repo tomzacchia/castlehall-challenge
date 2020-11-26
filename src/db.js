c63e48fed6db3c60c18b9bf0e0d38fcf4c6681b4
tomzacchia <tomzacchia@live.com>
2020-11-26 15:48:30 -0500

    Add more recipes to DB

--- src/db.js
+++ src/db.js
@@ -27,8 +27,18 @@ const server = new Server({
   routes() {
     this.namespace = "";
 
-    this.resource("recipe");
-    this.resource("ingredient");
+    // this.resource("recipe");
+    // this.resource("ingredient");
+    this.get("/recipes", (schema) => {
+      var recipes = schema.recipes.all();
+
+      var ingredientIDs = recipes.models[0].attrs.ingredients;
+      var ingredients = schema.ingredients.find(ingredientIDs);
+
+      recipes.models[0].attrs.ingredients = ingredients.models;
+
+      return recipes;
+    });
   },
 });
 
@@ -52,12 +62,58 @@ server.db.loadData({
       vegetarian: false,
       calories: 79,
     },
+    {
+      id: 4,
+      name: "hamburger patty",
+      vegeterian: false,
+      calories: 204,
+    },
+    {
+      id: 5,
+      name: "hamburger buns",
+      vegeterian: true,
+      calories: 263,
+    },
+    {
+      id: 6,
+      name: "lettuce",
+      vegeterian: true,
+      calories: 5,
+    },
+    {
+      id: 7,
+      name: "ketchup",
+      vegeterian: true,
+      calories: 19,
+    },
+    {
+      id: 8,
+      name: "tomatoes",
+      vegeterian: true,
+      calories: 22,
+    },
+    {
+      id: 9,
+      name: "ranch dressing",
+      vegeterian: true,
+      calories: 73,
+    },
   ],
   recipes: [
     {
       id: 1,
       name: "poutine",
       ingredients: [1, 2, 3],
+    },
+    {
+      id: 2,
+      name: "hamburger",
+      ingredients: [4, 5, 6, 7, 8],
+    },
+    {
+      id: 3,
+      name: "salad",
+      ingredients: [7, 8, 9],
     },
   ],
 });
