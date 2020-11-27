e08bb9310115146a34671fb1989d0d1beb247557
tomzacchia <tomzacchia@live.com>
2020-11-26 19:38:15 -0500

    Display recipe details after component mounts

--- src/pages/recipe-details/recipe-details.jsx
+++ src/pages/recipe-details/recipe-details.jsx
@@ -1,16 +1,34 @@
 import React, { useEffect, useState } from "react";
+import { CircularProgress } from "@material-ui/core";
 import api from "../../api";
 
 function RecipeDetailsPage({ id: recipeId }) {
-  // const [recipe, setRecipe] = useState(null);
+  const [recipe, setRecipe] = useState(null);
 
   useEffect(() => {
-    api.get(`/recipes/${recipeId}`).then((data) => {
-      console.log(data);
+    api.get(`/recipes/${recipeId}`).then(({ data }) => {
+      setRecipe(data.recipe);
     });
   }, [recipeId]);
 
-  return <div></div>;
+  return !recipe ? (
+    <CircularProgress />
+  ) : (
+    <div className="recipe-details">
+      <div className="image-container">
+        <div className="img"></div>
+        <p> {recipe.name}</p>
+      </div>
+      <div className="ingredients">
+        {recipe.ingredients.map((ingredient) => (
+          <p key={ingredient.id}>
+            {ingredient.name}{" "}
+            {ingredient.vegeration ? "vegeratian" : "not vegeterian"}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default RecipeDetailsPage;
