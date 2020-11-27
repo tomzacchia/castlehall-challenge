9d7eff69c5f0859feb67cb45c35d827fce8d30d3
tomzacchia <tomzacchia@live.com>
2020-11-27 11:49:09 -0500

    Update RecipeDetails UI

--- src/pages/recipe-details/recipe-details.jsx
+++ src/pages/recipe-details/recipe-details.jsx
@@ -11,9 +11,27 @@ import api from "../../api";
 import { useParams } from "react-router";
 
 const useStyles = makeStyles({
+  root: {
+    padding: "1rem",
+    marginLeft: "-250px",
+    width: 500,
+    position: "fixed",
+    top: 100,
+    left: "50%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
   media: {
     height: 300,
     width: 300,
+  },
+  spinner: {
+    marginLeft: "-125px",
+    width: 250,
+    position: "fixed",
+    top: 300,
+    left: "50%",
   },
 });
 
@@ -29,9 +47,9 @@ function RecipeDetailsPage() {
   }, [recipeId]);
 
   return !recipe ? (
-    <CircularProgress />
+    <CircularProgress className={classes.spinner} />
   ) : (
-    <Card>
+    <Card className={classes.root}>
       <CardMedia
         className={classes.media}
         image={recipe.imgUrl}
