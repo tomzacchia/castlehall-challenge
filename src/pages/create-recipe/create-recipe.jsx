e6328f336c6741e754cb4e2328e0d00dc9530db0
tomzacchia <tomzacchia@live.com>
2020-11-26 23:27:49 -0500

    Add useForm hook to CreateRecipe
    -  name={"ingredients." + ingredient.id}, "." in name gives us all checkbox values in an array attached to data
    - formatFormData returns array of ingredients that were selected

--- src/pages/create-recipe/create-recipe.jsx
+++ src/pages/create-recipe/create-recipe.jsx
@@ -1,14 +1,98 @@
+import {
+  Button,
+  Checkbox,
+  FormControlLabel,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import api from "../../api";
+import "./create-recipe.scss";
+
+/*
+imgUrl: string
+ingredients: [ingredientIds]
+name: string
+*/
 
 function CreateRecipe() {
   let [ingredients, setIngredients] = useState([]);
+  let { register, handleSubmit } = useForm();
+  // const classes = useStyles();
 
   useEffect(() => {
-    api.get("/ingredients").then(({ data }) => console.log(data));
+    api.get("/ingredients").then(({ data }) => {
+      setIngredients(data.ingredients);
+    });
   }, []);
 
-  return <div></div>;
+  function onSubmit(data) {
+    let formattedData = formatFormData({ ...data });
+  }
+
+  return (
+    <div className="create-recipe-page">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          margin="normal"
+          variant="outlined"
+          name="name"
+          id="name"
+          label="name"
+          inputRef={register}
+          required
+        />
+
+        <TextField
+          margin="normal"
+          variant="outlined"
+          name="imgUrl"
+          id="imgUrl"
+          label="Recipe Image URL"
+          inputRef={register}
+          required
+        />
+
+        <Typography variant="body2" component="p">
+          Ingredients
+        </Typography>
+
+        <div>
+          {ingredients.map((ingredient) => (
+            <FormControlLabel
+              control={
+                <Checkbox
+                  color="primary"
+                  name={"ingredients." + ingredient.id}
+                  inputRef={register}
+                  value={ingredient.id}
+                />
+              }
+              key={ingredient.id}
+              label={ingredient.name}
+            />
+          ))}
+        </div>
+
+        <Button variant="contained" color="primary" type="submit">
+          SUBMIT
+        </Button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateRecipe;
+
+function formatFormData(data) {
+  let newIngredients = data.ingredients
+    .filter((id) => {
+      if (id) return true;
+    })
+    .map((id) => parseInt(id));
+
+  data.ingredients = newIngredients;
+
+  return data;
+}
